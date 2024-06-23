c3789019274b135922f060ba541fa611be2dab2a
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-06-23 21:49:44 +0100

    start building neural network

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import cv2
 import imghdr
 from matplotlib import pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 import numpy as np
 
 
@@ -87,7 +89,20 @@ train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
 test = data.skip(train_size + val_size).take(test_size)
 
+# 3 - Building the Deep Neural Network
+model = Sequential()
 
+model.add(Conv2D(16, (3, 3), 1), activation='relu', input_shape=(256, 256, 3))
+model.add(MaxPooling2D())
 
+model.add(Conv2D(32, (3, 3), 1), activation='relu')
+model.add(MaxPooling2D())
+
+model.add(Conv2D(16, (3, 3), 1), activation='relu')
+model.add(MaxPooling2D())
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
 
